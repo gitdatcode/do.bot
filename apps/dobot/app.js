26d542d009c42b79f50affd0f9fdcf68a1c77594
mark <mark.henderson@8trk.com>
2017-04-14 22:17:14 +0000

    mapped /dobot help

--- apps/dobot/app.js
+++ apps/dobot/app.js
@@ -2,21 +2,27 @@ const command = require('../../controllers/command');
 
 const help = 'do.bot help application';
 
-command.handler.add('dobot', function(request, response){
+const commands = {
     /**
-     * this is defined on demand becuase there is no guarentee that this app
-     * will load after the others
+     * /dobot help
      */
-    let all_commands = [];
+    'help': function(input, request, response){
+        let all_commands = [];
 
-    for(var c in command.handler.commands){
-        if(command.handler.commands.hasOwnProperty(c)){
-            const help = command.handler.commands[c]['help'];
+        for(var c in command.handler.commands){
+            if(command.handler.commands.hasOwnProperty(c)){
+                const help = command.handler.commands[c]['help'];
 
-            all_commands.push(`${c}: ${help}`);
+                all_commands.push(`${c}: ${help}`);
+            }
         }
-    }
 
-    response.status(200);
-    response.send(all_commands.join('\n'));
-}, help);
+        response.status(200);
+        response.send(all_commands.join('\n'));
+    }
+};
+
+const command_parser = new command.StringArgumentParser(commands);
+
+command.handler.add('dobot', command_parser, help);
+
