3113b7c3c84febd550afdb8e80cdbc37e9b90082
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2017-04-14 17:49:03 -0500

    changes to exports code in buffit/

--- apps/buffit/index.js
+++ apps/buffit/index.js
@@ -3,26 +3,8 @@
   the DatCode Twitter account
   author: Jer'Maine Jones, Jr. @jermaine
 */
-const command = require('../controllers/command');
-const app = require('./app');
-//a short description of the app to be used for the /dobot command
-const help = "command to add resource posts to a Buffer account, which shares to the DatCode Twitter account."
-
+let run = require('./app').run;
 const buffit = {
-  //post function | used when a command is received (registered to the command handler)
-  post: (request, response) => {
-    //respond to the request. We'll do another response later
-    response.status(200);
-    response.send('Got it! Thanks.');
-
-    //text for the Buffer post
-    let text = request.body.text,
-    //id of user who sent the request
-    user = request.body.user,
-    //the channel to post back to
-    channels = channels.push(request.body.channel);
-    app.run(text, user, channels);
-  },
   //send function | used when another app wants to post to the Buffer account
   send: (text, user, channels) => {
     if (text === "" || user === "" || channels.length === 0)
@@ -30,13 +12,10 @@ const buffit = {
       console.log('(buffit/index.js : buffit.send) Something wrong with args. ')
     }
     else{
-      app.run(text, user, channels);
+      run(text, user, channels);
     }
   }
-
 };
 
 //export the buffit object as a module
 module.exports = buffit;
-//add the buffit app to the command handler
-command.handler.add('/buffit', buffit.post, help);
