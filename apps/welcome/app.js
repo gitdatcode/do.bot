99b46d6122f434a1edd76800167c8e504c3b17f8
= <=>
2017-04-23 21:42:37 +0000

    fixed welcome dm

--- apps/welcome/app.js
+++ apps/welcome/app.js
@@ -5,14 +5,14 @@
 */
 const event = require('../../controllers/event');
 
-const dmGreeting = "Welcome to the DatCode Slack group, we're glad you're here! Since you're new, just give a little hello in the <#sayhello> channel. Tell us \
+const dmGreeting = "Welcome to the DatCode Slack group, we're glad you're here! Since you're new, just give a little hello in the #sayhello channel. Tell us \
 where you're from, maybe what you do or what you're working on, and what you look to learn or gain within the community.\n\n\
-If you're new to coding, we'd highly suggest following the <#for-newbies> channel, where people can post coding resources, and ask for help in\
-starting their coding journey. If you're seasoned, or looking to grow in a particular area, join the <#for-amateurs> and/or the <#resources> channels as \
-well as the channels that interest you; all the coding channels correspond to a language (like <#htmlcss> or <#java>) or a platform (like <#ios> or <#android>). \
-There's also channels like <#random> and <#nowplaying> that are outside coding, just to talk about stuff.\n\n\
-If you need any help, feel free to ask in the <#general> channel. We'll help you out!\n\
-(If you're new to slack completely, you can ask <@slackbot> for help; and check this out for some tips and tricks: https://www.thrillist.com/tech/nation/slack-app-hacks-shortcuts-tips-tricks )"
+If you're new to coding, we'd highly suggest following the #for-newbies channel, where people can post coding resources, and ask for help in\
+starting their coding journey. If you're seasoned, or looking to grow in a particular area, join the #for-amateurs and/or the #resources channels as \
+well as the channels that interest you; all the coding channels correspond to a language (like #htmlcss or #java) or a platform (like #ios or #android). \
+There's also channels like #random and #nowplaying that are outside coding, just to talk about stuff.\n\n\
+If you need any help, feel free to ask in the #general channel. We'll help you out!\n\
+(If you're new to slack completely, you can ask @slackbot for help; and check this out for some tips and tricks: https://www.thrillist.com/tech/nation/slack-app-hacks-shortcuts-tips-tricks )"
 
 const generalGreeting = "!!!!!! NEW MEMBER !!!!!! Say hey to <@username>!"
 
@@ -26,7 +26,7 @@ const welcomeMessage = (request, response) => {
   const user = '@' + body.event.user;
 
   // dm the new user
-  request.slack.chat.postMessage(user, dmGreeting, "full", (err, response) => {
+  request.slack.chat.postMessage(user, dmGreeting, {'parse': 'full'},  (err, response) => {
     if (err){
       console.log("(welcome.js : welcomeMessage:request.slack.chat.postMessage) Error: ", err);
     }
