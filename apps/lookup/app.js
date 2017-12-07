8607ce73601c2de060a8494a658b581911c6a2cb
= <=>
2017-12-07 00:10:53 +0000

    editing the command in realtime

--- apps/lookup/app.js
+++ apps/lookup/app.js
@@ -9,17 +9,14 @@ command.handler.add(command_name, async function(request, response){
     let search = request.body.text;
 
     google(search, function(err, res){
-        if(err || !res.length){
-            let message = 'There was an error with your search'
+        if(err){
+            var message = 'There was an error with your search'
         }else{
-            let result = res[0],
+            var result = res.links[0],
                 description = result.description,
                 link = result.href,
                 title = result.title,
-                message = `The internet says ${search} means:\n
-                    *${title}*\n
-                    ${description}\n
-                    _${link}_`;
+                message = `The internet says _'${search}'_ means\n*${title}*\n${description}\n_${link}_`;
         }
 
         response.status(200);
